0fe7c5f5316e47b62daed056c6f0ad92a14f66be
liyanfei <yanfei.li@qq.com>
2019-12-04 13:52:53 +0800

    update image_download.py

--- image_download.py
+++ image_download.py
@@ -4,7 +4,7 @@ import requests
 import re
 
 
-def getWebimage(weburl,image_pattern="jpg"):
+def getWebimage(weburl, image_pattern="jpg"):
     """
     用于下载指定web页面的图片
     :param weburl: web页面对应的url
@@ -13,12 +13,14 @@ def getWebimage(weburl,image_pattern="jpg"):
     """
 
     # 设置匹配图片地址的表达式
-    reimg = r'https://\S*?\.%s' % image_pattern
+    reimg = r'http://\S*?\.%s' % image_pattern
     img_re = re.compile(reimg)
 
     # 读取指定的web页面内容
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
-    url = request.Request(url=weburl,headers=headers)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106"
+                      " Safari/537.36"}
+    url = request.Request(url=weburl, headers=headers)
     html = request.urlopen(url)
     s = str(html.read())
 
@@ -34,21 +36,23 @@ def getWebimage(weburl,image_pattern="jpg"):
         # 下载失败的图片数量
         fail_count = 0
         # 对图片列表中的图片逐一下载
+        success = False
         for img in img_list:
-            print("正在下载第%s个,共%s个..." % (index,len(img_list)))
+            print("正在下载第%s个,共%s个..." % (index, len(img_list)))
             try:
-                request.urlretrieve(img,r"C:\Users\Administrator\Desktop\img\{0}-{1}.{2}".format(now,index,image_pattern))
+                request.urlretrieve(img,
+                                    r"C:\Users\Administrator\Desktop\img\{0}-{1}.{2}".format(now, index, image_pattern))
                 success = True
             except Exception:
                 print("第%s个图片下载失败" % index)
-                success = False
                 index += 1
                 fail_count += 1
                 # 对于下载失败的，尝试用另一种方式重新下载
                 if not success:
                     print("重新尝试下载...")
                     res = requests.get(img)
-                    with open(r"C:\Users\Administrator\Desktop\img\{0}-{1}.{2}".format(now,(index-1),image_pattern), "wb") as f:
+                    with open(r"C:\Users\Administrator\Desktop\img\{0}-{1}.{2}".format(now, (index - 1), image_pattern),
+                              "wb") as f:
                         f.write(res.content)
                     print("下载成功...")
                     fail_count -= 1
@@ -61,6 +65,5 @@ def getWebimage(weburl,image_pattern="jpg"):
         print("此url界面无%s图片！" % image_pattern)
 
 
-
-if __name__ =="__main__":
-    getWebimage("https://zhidao.baidu.com/question/1447655141570242420.html")
+if __name__ == "__main__":
+    getWebimage("http://sucai.redocn.com/font/jpgtupian/")
